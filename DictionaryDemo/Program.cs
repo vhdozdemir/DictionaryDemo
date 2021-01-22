cf22f52b96cfd3e0f360e5f33babcc5949f2e749
vhdozdemir97 <vhdozdemir97@gmail.com>
2021-01-23 00:37:51 +0300

    Add project files.

---
+++ DictionaryDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DictionaryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> Category = new MyDictionary<int, string>();
+            Category.Add(1, "Ayakkabı");
+            Category.Add(2, "Çanta");
+            Category.Add(3, "Giyim");
+            Category.Add(4, "Aksesuar");
+            for (int i = 0; i < Category.Keys.Length; i++)
+            {
+                Console.WriteLine("Kategori Id: " + Category.Keys[i] + " - " + "Kategori Adı: " + Category.Values[i]);
+            }
+        }
+    }
+}
